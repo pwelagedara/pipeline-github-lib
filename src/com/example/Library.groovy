3591c314c81a835ea74d0317347350c7bca3edb5
Pubudu Welagedara <pwelagedara@virtusapolaris.com>
2018-02-22 12:47:36 -0500

    added docker functions

--- src/com/example/Library.groovy
+++ src/com/example/Library.groovy
@@ -1,11 +1,54 @@
 package com.example
 
+/*
+Git Functions
+*/
+
 def getCommitHash() {
+
   sh "git rev-parse --short HEAD > commit-hash.txt"
   readFile('commit-hash.txt').trim()
 }
 
+// You do not need this for Multibranch Pipeline Projects
 def getCurrentBranch() {
+
   sh "git rev-parse --abbrev-ref HEAD > current-branch.txt"
   readFile('current-branch.txt').trim()
+}
+
+/*
+Docker Functions
+*/
+
+def dockerTest() {
+
+  sh "docker --version";
+}
+
+def dockerBuild(Map args) {
+
+  def path
+
+  if (args.path == null) {
+      path = "./"
+  } else {
+      path = args.path
+  }
+
+  sh "docker build -t ${args.imageName}:${args.imageTag} ${path}"
+}
+
+def dockerTag(Map args) {
+
+  docker tag ${args.imageName}:${args.imageTag} ${args.targetImageName}:${args.targetImageTag}
+}
+
+/*
+Helm Functions
+*/
+
+def helmTest() {
+
+  sh "helm list"
 }
