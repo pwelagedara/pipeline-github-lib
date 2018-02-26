c911f648f59a404793bd480b0254735dd13eb09e
pwelagedara <pubuduwelagedara@gmail.com>
2018-02-25 23:07:31 -0500

    cleaning up

--- src/com/example/Library.groovy
+++ src/com/example/Library.groovy
@@ -1,23 +1,25 @@
 package com.example
 
 // Add Successful Builds to a Map and return
+/* Needs the following scripts to be approved
+*  method hudson.model.Actionable getActions
+*  method jenkins.plugins.git.AbstractGitSCMSource$SCMRevisionImpl getHash
+*  method jenkins.scm.api.SCMRevisionAction getRevision
+*  method org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper getRawBuild
+*/
 def getSuccessfulBuildsMap(currentBuild) {
-  def successfulBuilds = lastSuccessfullBuild(currentBuild.getPreviousBuild(), [:])
-  println 'sizeeee'
-  println successfulBuilds.size()
-  return successfulBuilds
+  lastSuccessfullBuild(currentBuild.getPreviousBuild(), [:])
 }
 
 // Reference https://support.cloudbees.com/hc/en-us/articles/217591038-How-to-Iterate-Through-the-Last-Successful-Builds-in-Pipeline-Job
 def lastSuccessfullBuild(build, successfulBuilds) {
     if(build != null){
         if(build.result == 'SUCCESS') {
-            println 'okayyyyyyyyy'
             successfulBuilds.put(build.number, getCommitHash(build))
         }
         lastSuccessfullBuild(build.getPreviousBuild(), successfulBuilds)
     }else {
-      return successfulBuilds
+      successfulBuilds
     }
  }
 
@@ -26,5 +28,5 @@ def lastSuccessfullBuild(build, successfulBuilds) {
 def getCommitHash(build) {
   def rawBuild = currentBuild.rawBuild
   def scmAction = rawBuild?.actions.find { action -> action instanceof jenkins.scm.api.SCMRevisionAction }
-  return scmAction?.revision?.hash
+  scmAction?.revision?.hash
 }
