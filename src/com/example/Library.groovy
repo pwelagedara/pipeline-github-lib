0adf556a4ae5db81ec78f2a6457d7d49ceaf4beb
pwelagedara <pubuduwelagedara@gmail.com>
2018-02-25 23:19:06 -0500

    added access modifiers

--- src/com/example/Library.groovy
+++ src/com/example/Library.groovy
@@ -12,10 +12,10 @@ def getSuccessfulBuildsMap(currentBuild) {
 }
 
 // Reference https://support.cloudbees.com/hc/en-us/articles/217591038-How-to-Iterate-Through-the-Last-Successful-Builds-in-Pipeline-Job
-def lastSuccessfullBuild(build, successfulBuilds) {
+private def lastSuccessfullBuild(build, successfulBuilds) {
     if(build != null){
         if(build.result == 'SUCCESS') {
-            successfulBuilds.put(build.number, getCommitHash(build).substring(0, 8)) // Short Version of the Hash
+            successfulBuilds.put(build.number, getCommitHash(build).substring(0, 7)) // Short Version of the Hash
         }
         lastSuccessfullBuild(build.getPreviousBuild(), successfulBuilds)
     }else {
@@ -25,7 +25,7 @@ def lastSuccessfullBuild(build, successfulBuilds) {
 
 //Reference https://gist.github.com/ftclausen/8c46195ee56e48e4d01cbfab19c41fc0
 @NonCPS
-def getCommitHash(build) {
+private def getCommitHash(build) {
   def rawBuild = currentBuild.rawBuild
   def scmAction = rawBuild?.actions.find { action -> action instanceof jenkins.scm.api.SCMRevisionAction }
   scmAction?.revision?.hash
